a68152ce1e7f7b4a87f6fa71a83b3616928e0471
Foulest <43710301+Foulest@users.noreply.github.com>
2025-04-18 17:14:03 -0700

    Added Switch.ch report page

--- src/main/pages/warning/WarningPage.js
+++ src/main/pages/warning/WarningPage.js
@@ -97,6 +97,12 @@ window.addEventListener("load", () => {
                     + encodedMaliciousUrl + "%0ADetected%20as%3A%20" + encodedResult
                     + "%0A%0AI%20believe%20this%20website%20is%20legitimate.%0A%0AThanks.");
 
+            case ProtectionResult.ResultOrigin.SWITCH_CH:
+                return new URL("mailto:info@switch.ch?subject=False%20Positive&body=Hello%2C%0A%0AI%20would%20like%20"
+                    + "to%20report%20a%20false%20positive.%0A%0AProduct%3A%20Switch.ch%20DNS%20Firewall%0AURL%3A%20"
+                    + encodedMaliciousUrl + "%0ADetected%20as%3A%20" + encodedResult
+                    + "%0A%0AI%20believe%20this%20website%20is%20legitimate.%0A%0AThanks.");
+
             default:
                 return null;
         }
